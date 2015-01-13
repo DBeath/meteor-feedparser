f8166c809892b2c1f5d8011cbd11b31acd1164f3
David Beath <davidgbeath@gmail.com>
2015-01-14 11:03:58 +1300

    Updated node-feedparser reference to v0.19.2

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
 	summary: "Robust RSS, Atom, and RDF feed parsing in Node.js"
 });
 
-Npm.depends({feedparser: "0.16.6"});
+Npm.depends({feedparser: "0.19.2"});
 
 Package.on_use(function(api){
 	api.add_files(['feedparser.js'], 'server');
